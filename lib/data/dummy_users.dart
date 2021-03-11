98331e54654011990099a95d9f814bcdbc17cb79
rebecca martinho <rebeccamartinho@hotmail.com>
2021-03-10 23:32:17 -0300

    update

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Rebecca',
+      email: 'rebecca@rebecca.com.br',
+      avatarUrl:
+          'https://avatars.githubusercontent.com/u/39964020?s=400&u=889f59804076be9ab85e067b0130cdfda121f6e4&v=4'),
+  '2': const User(
+      id: '2',
+      name: 'Lucas',
+      email: 'lucas@neguinha.com.br',
+      avatarUrl:
+          'https://avatars.githubusercontent.com/u/30658655?s=400&u=ce1699d469edb88e10bf2abf04c1ddd9a2e5ee24&v=4'),
+  '3': const User(
+      id: '3',
+      name: 'Luan',
+      email: 'luan@baiano.com.br',
+      avatarUrl:
+          'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/cc/cc20a374495f727199fcaaf28db65ada15069956_full.jpg'),
+};
