3fca0ed1a29e19a14277bade9cc578cc80c94e09
cherobim22@gmail.com <lucascherobim@hotmail.com>
2022-10-17 22:35:13 -0300

    adding home page

---
+++ lib/views/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/components/user_tile.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:provider/provider.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Users users = Provider.of(context);
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.USER_FORM);
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Column(children: [
+            Text("Bem-vindo"),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed(AppRoutes.USER_LIST);
+                },
+                child: Text("Ver minha lista"))
+          ]),
+        ));
+  }
+}
